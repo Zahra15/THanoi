c81b99acc27f8a01c7eebd2c72dffc6f3898b608
Me <Me@Lenovo-PC>
2017-01-10 11:27:13 +0300

    commments

--- src/thanoi/Tower.java
+++ src/thanoi/Tower.java
@@ -35,40 +35,5 @@ public class Tower extends JComponent{
     
         g.setColor(Color.black);
         g.fillRect (630, 50, 20, 250); 
-        
-        
-       /* Graphics2D g2 = (Graphics2D) g;
-        
-        Rectangle2D.Double disk1  = new Rectangle2D.Double(60,280, 200, 20);
-        g2.setColor(Color.blue);
-        g2.fill(disk1);
-        
-        Rectangle2D.Double disk2  = new Rectangle2D.Double(80,260, 160, 20);
-        g2.setColor(Color.red);
-        g2.fill(disk2);
-        
-        Rectangle2D.Double disk3  = new Rectangle2D.Double(100,240, 120, 20);
-        g2.setColor(Color.yellow);
-        g2.fill(disk3);
-        
-         Rectangle2D.Double disk4  = new Rectangle2D.Double(120,220, 80, 20);
-        g2.setColor(Color.orange);
-        g2.fill(disk4);
-        //disk4.setRect(370,280, 80, 20);
-        disk4.setFrame(370,280, 80, 20);
-        g2.setColor(Color.black);
-        g2.fillRect(150, 220, 20, 20);
-        g2.setColor(new Color(242, 242, 242));
-        g2.fillRect(120, 220, 30, 20);
-        g2.fillRect(170, 220, 30, 20);
-        //g2.drawRect(150, 50, 20, 250);
-         g2.setColor(Color.orange);
-        g2.fill(disk4);*/
-    }
-    
-     public void getY(int x, int y){
-       JComponent co = (JComponent) getComponentAt(x,y);
-       Color coC = co.getBackground();
-    }
 }
- 
+}
